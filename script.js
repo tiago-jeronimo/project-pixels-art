6e3d07ebf8237797a0f5316ff03f9ac0c3734537
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-01-24 23:53:46 -0300

    Requisito 6 - Feito, tive ajuda do @Leandro Paiva ate agora

---
+++ script.js
@@ -0,0 +1,9 @@
+const color = document.querySelector('#color-palette');
+
+function colorPixel(pixel){
+  pixel.style.backgroundColor = penColor;    
+}
+const penColor = 'black';
+function setPenColour(color) {
+    penColor = color;
+}
